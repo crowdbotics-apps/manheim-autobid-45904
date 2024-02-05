3e814af746d3a4e4593418e1d2289a8f0fc390dc
Crowdbotics <team@crowdbotics.com>
2024-02-05 15:42:15 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "manheim-autobid-45904",
+  url: "https://manheim-autobid-45904.botics.co",
+  api: "https://manheim-autobid-45904.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
